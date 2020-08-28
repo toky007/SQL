6a0ce57baa6d8d8b4757c7602a44de691e5347d7
toky007 <raftoky96@gmail.com>
2020-08-28 19:40:08 +0300

    commit

---
+++ world.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE 'world';
+
+CREATE TABLE personnes(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nom TEXT,
+  prenom TEXT,
+  age INT,
+  taille DECIMAL,
+  poids DECIMAL
+);
+
+INSERT INTO personnes(nom, prenom, age, taille, poids) VALUES 
+('Rakoto', 'Be nify', 10, 1.50 , 70.0),
+('Lava', 'Rapeto', 25, 2.0, 30.0), 
+('Ba', 'Lita', 7, 1.0, 20.5), 
+('Kiala', 'Manjakely', 100, 1.68, 45.7),
+('Kiala', 'Pota', 37, 0.8, 50.0);
+
+ALTER TABLE personnes ADD couleur_preferee TEXT;
+
+UPDATE personnes SET couleur_preferee = 'rouge' WHERE id = 1;
+UPDATE personnes SET couleur_preferee = 'vert' WHERE id = 2;
+UPDATE personnes SET couleur_preferee = 'jaune' WHERE id = 3;
+UPDATE personnes SET couleur_preferee = 'violet' WHERE id = 4;
+UPDATE personnes SET couleur_preferee = 'grise' WHERE id = 5;
+
+
